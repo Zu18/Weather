c9efdf3f1a06d1dc75c787bddeef8c2358582d31
Zukhra <zuhra.galieva.2014@gmail.com>
2023-03-28 12:41:34 +0100

    Add the empty templetes for the future pages of the app

--- weather/urls.py
+++ weather/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, TodayPageView, FuturePageView, PastPageView
 
 urlpatterns = [
+    path('past/', PastPageView.as_view(), name='past'),
+    path('future/', FuturePageView.as_view(), name='future'),
+    path('today/',  TodayPageView.as_view(), name='today'),
     path('', HomePageView.as_view(), name='home'),
 ]
--- weather/views.py
+++ weather/views.py
@@ -4,3 +4,15 @@ from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     template_name = "home.html"
+
+
+class TodayPageView(TemplateView):
+    template_name = "today.html"
+
+
+class FuturePageView(TemplateView):
+    template_name = "future.html"
+
+
+class PastPageView(TemplateView):
+    template_name = "past.html"
